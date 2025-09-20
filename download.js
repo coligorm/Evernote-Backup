a9aeb3a930c7d54e8b79bff303c2cf413d621dd5
coligorm <colinrhysgorman@gmail.com>
2025-09-21 01:27:25 +1000

    Added File download button and regex manipulation

--- download.js
+++ download.js
@@ -1,8 +1,26 @@
 downloadBtn.addEventListener('click', () => {
-  const blob = new Blob([window.fileContents], {type: 'text/plain'});
+  // Manipulate the file contents here
+  let manipulatedContent = window.fileContents;
+
+  // Example: Remove all HTML tags
+  manipulatedContent = manipulatedContent.replace(/<b>/g, '#');
+  manipulatedContent = manipulatedContent.replace(/<li[^>]*<\/div>/g, '\n- ');
+  manipulatedContent = manipulatedContent.replace(/<\/div>/g, '\n');
+  
+  manipulatedContent = manipulatedContent.replace(/<[^>]*>/g, '');
+
+  // Example: Add a header
+  manipulatedContent = '--- Converted Content ---\n' + manipulatedContent;
+
+  // Download as .txt file
+  const blob = new Blob([manipulatedContent], {type: 'text/plain'});
   const downloadLink = document.createElement('a');
   downloadLink.href = URL.createObjectURL(blob);
-  downloadLink.download = window.fileName;
+
+  // Force .txt extension
+  let downloadName = window.fileName.replace(/\.[^/.]+$/, '') + '.txt';
+  downloadLink.download = downloadName;
+
   document.body.appendChild(downloadLink);
   downloadLink.click();
   document.body.removeChild(downloadLink);
